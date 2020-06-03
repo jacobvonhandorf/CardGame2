9dd4b07d20948670661ebfb7afb268c2ddcd88e9
Jacob Von Handorf <jacobvonhandorf@gmail,com>
2020-06-03 14:22:52 -0400

    Fix Scroll View Effect Collider

--- Card Game/Assets/testScript.cs
+++ Card Game/Assets/testScript.cs
@@ -5,11 +5,28 @@ using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class testScript : Deck
+public class testScript : MonoBehaviour, CanReceivePickedCards
 {
+    [SerializeField] CardPicker cardPicker;
+
+    public void receiveCardList(List<Card> cardList)
+    {
+        Debug.Log("Submitted");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+
+        // make list of cards
+        List<Card> cardList = new List<Card>();
+        for (int i = 0; i < 10; i++)
+        {
+            Card c = ResourceManager.Get().instantiateCardById(76);
+            c.removeGraphicsAndCollidersFromScene();
+            cardList.Add(c);
+        }
+
+        cardPicker.setUp(cardList, this, 2, 4, "Testing :)");
     }
 }
