b3231ed4bca207aa9ff566511a08b57fc210709e
Jacob Von Handorf <jacobvonhandorf@gmail,com>
2020-07-29 19:57:25 -0400

    Remove all old card prefabs

---
+++ Card Game/Assets/Scripts/Cards/Effects/FairyMotivatorEffs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyMotivatorEffs : CreatureEffects
+{
+    public override EventHandler onDeploy => onDeployEffect;
+
+    private void onDeployEffect(object sender, EventArgs e)
+    {
+        foreach (Card c in creature.controller.hand.getAllCardsWithType(Card.CardType.Creature))
+        {
+            (c as CreatureCard).creature.AttackStat += 1;
+            (c as CreatureCard).creature.Health += 1;
+        }
+    }
+}
